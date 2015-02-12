96c3f8de54c6eaffe4b443d6f8fad9f935c4ba9e
Michail Gagauz <mg@kupivip.ru>
2015-02-12 17:19:39 +0300

    Task #4 Refactoring of DateBindingFactory.java

--- src/main/java/com/gagauz/tapestry/binding/DateBindingFactory.java
+++ src/main/java/com/gagauz/tapestry/binding/DateBindingFactory.java
@@ -1,7 +1,5 @@
 package com.gagauz.tapestry.binding;
 
-import java.util.Date;
-
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.apache.tapestry5.Binding;
 import org.apache.tapestry5.BindingConstants;
@@ -11,12 +9,31 @@ import org.apache.tapestry5.ioc.services.TypeCoercer;
 import org.apache.tapestry5.services.BindingFactory;
 import org.apache.tapestry5.services.BindingSource;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class DateBindingFactory implements BindingFactory {
 
     private static final String DELIMITER = ",";
     private final BindingSource bindingSource;
     private final TypeCoercer resolver;
-    private final FastDateFormat format = FastDateFormat.getInstance("dd.MM.yyyy HH:mm");
+    private static FastDateFormat DEFAULT_FORMAT;
+    private static final Map<String, FastDateFormat> FORMAT_MAP = new HashMap<String, FastDateFormat>();
+    static {
+        DEFAULT_FORMAT = getOrCreate("dd.MM.yyyy HH:mm");
+    }
+
+    private static FastDateFormat getOrCreate(String format) {
+        FastDateFormat result = FORMAT_MAP.get(format);
+        if (null == result) {
+            synchronized (FORMAT_MAP) {
+                result = FastDateFormat.getInstance(format);
+                FORMAT_MAP.put(format, result);
+            }
+        }
+        return result;
+    }
 
     public DateBindingFactory(BindingSource bindingSource, TypeCoercer resolver) {
         this.bindingSource = bindingSource;
@@ -30,6 +47,7 @@ public class DateBindingFactory implements BindingFactory {
         return new AbstractContextBinding(bindingSource, resolver, description, container) {
             @Override
             public Object get() {
+                FastDateFormat format = parts.length == 1 ? DEFAULT_FORMAT : getOrCreate(parts[1]);
                 Date conditionValue = getValue(parts[0], BindingConstants.PROP, Date.class);
                 if (conditionValue != null) {
                     return format.format(conditionValue);
