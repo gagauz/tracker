45602b72ad94627d77ce859a8c37b927a25cb7d4
Miga <miga@miga.com>
2016-09-25 14:15:42 +0300

    BIg refactoring

--- src/main/java/com/gagauz/tracker/db/model/AccessRole.java
+++ src/main/java/com/gagauz/tracker/db/model/AccessRole.java
@@ -1,8 +1,14 @@
 package com.gagauz.tracker.db.model;
 
-public enum AccessRole {
-    ANONYMOUS,
-    ADMIN;
+public enum Roles {
+    NONE,
+    VIEWER,
+    ADMIN,
+    PROJECT_ADMIN,
+    PROJECT_USER,
+    USER_STORY_CREATOR,
+    TASK_CREATOR,
+    TASK_ASSIGNER,
+    BUG_CREATOR;
 
-    public static final AccessRole[] EMPTY_ROLES = new AccessRole[0];
 }
