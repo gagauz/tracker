2758628a7b426bebe8bc23015e6b13342dd4ca4c
Mihail Gagauz <Mihail_Gagauz@epam.com>
2016-09-26 13:08:31 +0300

    Fix

--- src/main/java/com/gagauz/tracker/db/model/FeatureVersion.java
+++ src/main/java/com/gagauz/tracker/db/model/FeatureVersion.java
@@ -16,77 +16,86 @@ import org.hibernate.annotations.ForeignKey;
 @Table(name = "feature_version")
 public class FeatureVersion implements Serializable {
 
-    @Embeddable
-    public static class FeatureVersionId implements Serializable {
-        private static final long serialVersionUID = 1939148011273312467L;
-        private Feature feature;
-        private Version version;
+	@Embeddable
+	public static class FeatureVersionId implements Serializable {
+		private static final long serialVersionUID = 1939148011273312467L;
+		private Feature feature;
+		private Version version;
 
-        @ForeignKey(name = "fk_featureVersion_feature")
-        @ManyToOne(fetch = FetchType.LAZY)
-        public Feature getFeature() {
-            return feature;
-        }
+		@ForeignKey(name = "fk_featureVersion_feature")
+		@ManyToOne(fetch = FetchType.LAZY)
+		public Feature getFeature() {
+			return feature;
+		}
 
-        public void setFeature(Feature feature) {
-            this.feature = feature;
-        }
+		public void setFeature(Feature feature) {
+			this.feature = feature;
+		}
 
-        @ForeignKey(name = "fk_featureVersion_version")
-        @ManyToOne(fetch = FetchType.LAZY)
-        public Version getVersion() {
-            return version;
-        }
+		@ForeignKey(name = "fk_featureVersion_version")
+		@ManyToOne(fetch = FetchType.LAZY)
+		public Version getVersion() {
+			return version;
+		}
 
-        public void setVersion(Version version) {
-            this.version = version;
-        }
+		public void setVersion(Version version) {
+			this.version = version;
+		}
 
-    }
+		@Override
+		public boolean equals(Object obj) {
+			if (null == obj || this == obj || !(obj instanceof FeatureVersionId)) {
+				return this == obj;
+			}
+			FeatureVersionId other = (FeatureVersionId) obj;
+			return other.getFeature().equals(feature) && other.getVersion().equals(version);
+		}
 
-    private static final long serialVersionUID = -8693198398126115278L;
-    private FeatureVersionId id;
-    private User creator;
+	}
 
-    @EmbeddedId
-    public FeatureVersionId getId() {
-        if (null == id) {
-            id = new FeatureVersionId();
-        }
-        return id;
-    }
+	private static final long serialVersionUID = -8693198398126115278L;
+	private FeatureVersionId id;
+	private User creator;
 
-    public void setId(FeatureVersionId id) {
-        this.id = id;
-    }
+	@EmbeddedId
+	public FeatureVersionId getId() {
+		if (null == id) {
+			id = new FeatureVersionId();
+		}
+		return id;
+	}
 
-    @ForeignKey(name = "fk_featureVersion_creator")
-    @ManyToOne(fetch = FetchType.LAZY)
-    public User getCreator() {
-        return creator;
-    }
+	public void setId(FeatureVersionId id) {
+		this.id = id;
+	}
 
-    public void setCreator(User creator) {
-        this.creator = creator;
-    }
+	@ForeignKey(name = "fk_featureVersion_creator")
+	@ManyToOne(fetch = FetchType.LAZY)
+	public User getCreator() {
+		return creator;
+	}
 
-    @Transient
-    public Project getProject() {
-        return getId().getFeature().getProject();
-    }
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
 
-    @Transient
-    public Version getVersion() {
-        return getId().getVersion();
-    }
+	@Transient
+	public Project getProject() {
+		return getId().getFeature().getProject();
+	}
 
-    @Transient
-    public Feature getFeature() {
-        return getId().getFeature();
-    }
+	@Transient
+	public Version getVersion() {
+		return getId().getVersion();
+	}
 
-    @Override
-    public String toString() {
-        return "FeatureVersion <feature=" + id.getFeature() + ", version=" + id.getVersion() + ">";
-    }
+	@Transient
+	public Feature getFeature() {
+		return getId().getFeature();
+	}
+
+	@Override
+	public String toString() {
+		return "FeatureVersion <feature=" + id.getFeature() + ", version=" + id.getVersion() + ">";
+	}
 }
--- src/main/java/com/gagauz/tracker/utils/AppProperties.java
+++ src/main/java/com/gagauz/tracker/utils/AppProperties.java
@@ -1,8 +1,8 @@
 package com.gagauz.tracker.utils;
 
-import org.gagauz.utils.IJavaProperty;
+import org.gagauz.utils.IAppProperty;
 
-public enum AppProperties implements IJavaProperty<AppProperties> {
+public enum AppProperties implements IAppProperty<AppProperties> {
     JDBC_USERNAME("b4f"),
     JDBC_PASSWORD("office"),
     JDBC_URL("jdbc:mysql://localhost:3306/tracker"),
@@ -18,7 +18,7 @@ public enum AppProperties implements IJavaProperty<AppProperties> {
     }
 
     @Override
-    public String getDefaultValue() {
+	public String getDefaultValue() {
         return defaultValue;
     }
 
